0ac169ef746b4fead89c1c531e6feb0c957dbbb1
Владимир Агапов <rigroll@yandex.ru>
2023-03-11 17:43:32 +0300

    Четвертое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,31 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
-
+  
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
-
+  
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.marks == undefined) {
+      return 0 
+    }
+      this.marks.push(...marks);
 }
-
+  
 Student.prototype.getAverage = function () {
-  
+    if ((this.marks == undefined) || (this.marks.length == 0)) {
+      return 0
+    }
+    let avg = this.marks.reduce((acc, item) => acc + item / this.marks.length, 0);
+    return avg;
 }
-
-Student.prototype.exclude = function (reason) {
   
+Student.prototype.exclude = function (reason) {
+    delete this.marks;
+    delete this.subject;
+    this.excluded = reason;
 }
